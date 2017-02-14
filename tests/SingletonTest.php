0480cac814c6fc7f673193ccf135c4a413c1b9ed
Riccardo Deri <riccardoallegro@gmail.com>
2017-02-14 18:50:33 +0100

    Patterns:     Abstract Factory Method     Singleton

---
+++ tests/SingletonTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use patterns\singleton\Singleton;
+
+class SingletonTest extends TestCase
+{
+    public function testCanCreate()
+    {
+        $single = Singleton::GetInstance();
+
+        $this->assertNotNull($single);
+    }
+
+    public function testReturnTheSameInstance()
+    {
+        $single1 = Singleton::GetInstance();
+        $single2 = Singleton::GetInstance();
+
+        $this->assertEquals($single1, $single2);
+        $this->assertEquals($single1->getHash(), $single2->getHash());
+
+        $single1->setProperty(1001);
+        $this->assertEquals($single1->getProperty(), $single2->getProperty());
+    }
+}
+?>
